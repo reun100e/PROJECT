f65b849cca59804ddae3424a1bcb0fa07881c7bd
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2023-03-10 01:30:40 +0530

    HTML and CSS in depth Peer-graded Assignment Retail: Lucky Shrub Uplaoding first time

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+  const loader = document.querySelector("#loader");
+  const content = document.querySelector("#content");
+  loader.style.display = "none";
+  content.style.display = "block";
+});
