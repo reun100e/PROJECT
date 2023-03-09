f795a899660be04a923ac8d63a07dc190df355c0
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2023-03-10 02:07:52 +0530

    Added comments to all the code files.

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ window.addEventListener("load", function () {
   loader.style.display = "none";
   content.style.display = "block";
 });
+
+
+// Code for loading animation and revealing webpage when it finsh loading.
+
+// The above event listener function runs the fucntion passed 2nd argument when the webpage loads completely.
+// The 2nd function changes the CSS display style of loaded to "none" and content to "block".
